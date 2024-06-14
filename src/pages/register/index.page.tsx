a154c8f76e0b160514fac3d3fe02ae93bd3b4d6c
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-06-14 20:52:28 -0300

    feat: setando cookies

--- src/pages/register/index.page.tsx
+++ src/pages/register/index.page.tsx
@@ -1,5 +1,6 @@
 import { zodResolver } from '@hookform/resolvers/zod'
 import { Button, Heading, MultiStep, Text, TextInput } from '@ignite-ui/react'
+import { AxiosError } from 'axios'
 import { useRouter } from 'next/router'
 import { ArrowRight } from 'phosphor-react'
 import { useEffect } from 'react'
@@ -48,8 +49,13 @@ export default function Register() {
         name: data.name,
         username: data.username,
       })
-    } catch (error) {
-      console.log(error)
+    } catch (err) {
+      if (err instanceof AxiosError && err?.response?.data?.message) {
+        alert(err.response.data.message)
+        return
+      }
+
+      console.error(err)
     }
   }
 
