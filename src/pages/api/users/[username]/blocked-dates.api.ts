f322823328ffc23bac2023e47e9ffdf9329197ac
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-07-03 22:06:18 -0300

    feat: Finalizando query de dias lotados

--- src/pages/api/users/[username]/blocked-dates.api.ts
+++ src/pages/api/users/[username]/blocked-dates.api.ts
@@ -1,4 +1,3 @@
-// import dayjs from 'dayjs'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { prisma } from '../../../../lib/prisma'
 
@@ -26,7 +25,7 @@ export default async function handler(
   if (!user) {
     return res.status(400).json({ message: 'User does not exist.' })
   }
-  // buscar os dias da semana que o usuário tem disponibilidade
+
   const availableWeekDays = await prisma.userTimeInterval.findMany({
     select: {
       week_day: true,
@@ -42,13 +41,27 @@ export default async function handler(
     )
   })
 
-  // retornar os dias que estão cheios
-  const blockedDatesRaw = await prisma.$queryRaw`
-    SELECT *
+  const blockedDatesRaw: Array<{ date: number }> = await prisma.$queryRaw`
+    SELECT
+      EXTRACT(DAY FROM S.DATE) AS date,
+      COUNT(S.date) AS amount,
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60) AS size
+
     FROM schedulings S
+
+    LEFT JOIN user_time_intervals UTI
+      ON UTI.week_day = WEEKDAY(DATE_ADD(S.date, INTERVAL 1 DAY))
+
     WHERE S.user_id = ${user.id}
       AND DATE_FORMAT(S.date, "%Y-%m") = ${`${year}-${month}`}
+
+    GROUP BY EXTRACT(DAY FROM S.DATE),
+      ((UTI.time_end_in_minutes - UTI.time_start_in_minutes) / 60)
+
+    HAVING amount >= size
   `
 
-  return res.json({ blockedWeekDays, blockedDatesRaw })
+  const blockedDates = blockedDatesRaw.map((item) => item.date)
+
+  return res.json({ blockedWeekDays, blockedDates })
 }
