eb6e42b91e1242e54748dc7b035c98098d6c7b0c
Vinicius Ribeiro <viniciusgoes159@gmail.com>
2024-07-07 10:25:49 -0300

    feat: otimizações de SEO

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,6 +4,7 @@ import { SessionProvider } from 'next-auth/react'
 import { globalStyles } from '../styles/global'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { queryClient } from '../lib/react-query'
+import { DefaultSeo } from 'next-seo'
 
 globalStyles()
 
@@ -13,9 +14,17 @@ export default function App({
 }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
-  </QueryClientProvider>
+      <SessionProvider session={session}>
+        <DefaultSeo
+          openGraph={{
+            type: 'website',
+            locale: 'pt_BR',
+            url: 'https://www.ignite-call.rocketseat.com.br',
+            siteName: 'Ignite Call',
+          }}
+        />
+        <Component {...pageProps} />
+      </SessionProvider>
+    </QueryClientProvider>
   )
 }
